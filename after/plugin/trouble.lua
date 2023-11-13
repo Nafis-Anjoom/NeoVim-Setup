5fb1d202df74cb747a4b278dc87c8a2970bcc8a1
Nafis Anjoom <nafisanjoom@gmail.com>
2023-11-12 21:37:56 -0800

    trouble integration

---
+++ after/plugin/trouble.lua
@@ -0,0 +1,11 @@
+local trouble = require("trouble")
+
+vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end)
+vim.keymap.set("n", "<leader>xw", function() trouble.toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() trouble.toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() trouble.toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end)
+vim.keymap.set("n", "gr", function() trouble.toggle("lsp_references") end)
+vim.keymap.set("n", "gd", function() trouble.toggle("lsp_definitions") end)
+
+trouble.setup()
